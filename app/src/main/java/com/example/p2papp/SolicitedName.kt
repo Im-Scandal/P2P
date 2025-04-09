5757c9ff372685b894a046246e4b334f011988c7
Sergio Galindo <141683974+Im-Scandal@users.noreply.github.com>
2025-04-09 03:15:03 -0500

    AppsWifiDiscover - SolicitudNombre 2.0

--- app/src/main/java/com/example/p2papp/SolicitedName.kt
+++ app/src/main/java/com/example/p2papp/SolicitedName.kt
@@ -6,11 +6,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class SolicitedName: AppCompatActivity()
 {
     private lateinit var nextButton: Button
     private lateinit var nameText: EditText
+    private lateinit var infoButton: Button
 
     companion object{
         var nameUser: String = ""
@@ -18,11 +20,18 @@ class SolicitedName: AppCompatActivity()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.solicitud_nombre)
+        setContentView(R.layout.registro_inicio)
 
         nextButton = findViewById(R.id.nextButton)
         nameText = findViewById(R.id.nameWText)
+        infoButton = findViewById(R.id.infoView)
 
+        nameText.setOnClickListener{
+            nameText.setBackgroundResource(R.drawable.registro_nombre)
+            nameText.setHintTextColor(
+                ContextCompat.getColor(this, R.color.hint))
+            nameText.setHint(R.string.name_register)
+        }
         nextButton.setOnClickListener{
             if (nameText.text.isNotEmpty()){
                 nameUser = nameText.text.toString()
@@ -33,7 +42,15 @@ class SolicitedName: AppCompatActivity()
                 startActivity(intent)
             }else {
                 Toast.makeText(this, "Por favor ingrese su nombre", Toast.LENGTH_LONG).show()
+                nameText.setBackgroundResource(R.drawable.registro_nombre_empty)
+                nameText.setHintTextColor(
+                    ContextCompat.getColor(this, R.color.rojo_falla))
+                nameText.setHint("Por favor completa la casilla")
             }
         }
+        infoButton.setOnClickListener{
+            
+        }
+
     }
 }
